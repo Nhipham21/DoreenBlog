faddca675e346a49801f014674f2e38686831b58
Nhipham21 <19520816@gm.uit.edu.vn>
2023-05-20 21:28:07 +0700

    add login system and profile page

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,8 +13,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home')
+            messages.success(
+                request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
